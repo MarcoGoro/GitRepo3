260eaae43f50091d9a3b8a60b4cbeca65aa9e321
Utente <Utente@UTENTE-PC>
2018-01-21 19:38:15 +0100

    nuovo branches

--- Solution_03/ConsoleApplication1/Program.cs
+++ Solution_03/ConsoleApplication1/Program.cs
@@ -10,8 +10,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ciao sono Marco!");
             int[] array = new int[] { 6, 3, 9, 8 };
             int tmp = 0;
+
             for (int i = 0; i < array.Length; i++)
             {
                 for (int j = 0; j < array.Length; j++)
